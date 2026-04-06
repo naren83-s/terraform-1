ac7c454cc2b2b2a76199b3050a663ab25e26ebf4
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-04-06 14:03:44 +0400

    instance creation

---
+++ terraform-aws-instances/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type=string
+}
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "sg_id" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map
+  default = {}
+}
